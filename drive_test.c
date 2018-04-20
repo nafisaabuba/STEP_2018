ec6cf3b583f67699d353b2e3d1b13e1738847417
Joe Suzuki <joesuzuki516@gmail.com>
2018-04-20 16:34:57 -0400

    4  wheel drive train

--- drive_test.c
+++ drive_test.c
@@ -1,22 +1,43 @@
-#pragma config(Motor,  port2,           frontRightMotor,    tmotorVex393_MC29, openLoop)
-#pragma config(Motor,  port3,           frontLeftMotor,     tmotorVex393_MC29, openLoop)
-#pragma config(Motor,  port4,           backRightMotor,     tmotorVex393_MC29, openLoop)
-#pragma config(Motor,  port5,           backLeftMotor,     tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port2,           frontRightMotor, tmotorNormal, openLoop)
+#pragma config(Motor,  port3,           backRightMotor, tmotorNormal, openLoop)
+#pragma config(Motor,  port4,           frontLeftMotor, tmotorNormal, openLoop)
+#pragma config(Motor,  port5,           backLeftMotor, tmotorNormal, openLoop)
 
+/*----------------------------------------------------------------------------------------------------*\
+|*                                   - Moving Forward with 4 Motors -                                 *|
+|*                                      
+|*    MOTORS & SENSORS:                                                                               *|
+|*    [I/O Port]          [Name]                [Type]                [Description]                   *|
+|*    Motor Port 2        frontRightMotor       VEX 3-wire module     Right side motor                *|
+|*    Motor Port 3        backRightMotor        VEX 3-wire module     Right side motor                *|
+|*    Motor Port 4        frontLefttMotor       VEX 3-wire module     Left side motor                 *|
+|*    Motor Port 5        backLeftMotor         VEX 3-wire module     Left side motor                 *|
+\*----------------------------------------------------------------------------------------------------*/
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++| MAIN |+++++++++++++++++++++++++++++++++++++++++++++++
 task main()
-{
-	while(true)
- 	{
-	  	motor[frontRightMotor] = vexRT[Ch2]; 
-  		motor[frontLeftMotor] = vexRT[Ch3];
+{	  	
+	while (1 == 1)
+	{	
+		motor[frontRightMotor] = vexRT[Ch1];
+		motor[backRightMotor] = vexRT[Ch1];
+		motor[frontLeftMotor] = vexRT[Ch1];
+		motor[backLeftMotor] = vexRT[Ch1];
 
- 	//if(vexRT[Btn8U] == 1)
- 	//{
- 	//	motor[frontRightMotor] = 40;
- 	//}
- 	//else if(vexRT[Btn8D] == 1)
- 	//{
- 	//	motor[frontRightMotor] = -40;
- 	//}
+		if (vexRT[Ch1] == 1)
+		{
+			motor[frontRightMotor] = 127;
+			motor[backRightMotor] = 127;
+			motor[frontLeftMotor] = 127;
+			motor[backLeftMotor] = 127;
+		} else
+		{
+			motor[frontRightMotor] = 0;
+			motor[backRightMotor] = 0;
+			motor[frontLeftMotor] = 0;
+			motor[backLeftMotor] = 0;
+		}
 	}
-}
+}			
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
